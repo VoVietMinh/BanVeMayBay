5739acb4098c28e810a76f2dce915db05a2ec3f7
VoVietMinh <vietminh2222@gmail.com>
2016-11-08 23:23:39 +0700

    Create project

---
+++ ProjectBanNuoc/ProjectBanNuoc/App_Start/FilterConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectBanNuoc
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+
+    }
+}
---
+++ ProjectBanNuoc/ProjectBanNuoc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectBanNuoc.Startup))]
+namespace ProjectBanNuoc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
